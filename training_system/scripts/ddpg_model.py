20d13bc32fafa5c5d45ed75946000db3f36f441b
A S Mastrogeorgiou <amast@mail.ntua.gr>
2020-03-22 02:31:44 +0200

    Formatted Python files

--- training_system/scripts/ddpg_model.py
+++ training_system/scripts/ddpg_model.py
@@ -1,29 +1,23 @@
-"""Deep Determinitic Policy Gradient (DDPG) Network & Agent
-1) Based on  Max Lapan's implementation in Python in his book Deep Reinforcement Learning Hands-On:
-https://github.com/PacktPublishing/Deep-Reinforcement-Learning-Hands-On
-
-2) Using Pytorch 
-3) Usin PTAN (PyTorch AgentNet) package for RL 
-https://github.com/Shmuma/ptan
 """
-
+Deep Determinitic Policy Gradient (DDPG) Network & Agent based on:
+1) Max Lapan's implementation in the book Deep Reinforcement Learning Hands-On: https://github.com/PacktPublishing/Deep-Reinforcement-Learning-Hands-On
+2) Pytorch 
+3) PTAN (PyTorch AgentNet) package for RL https://github.com/Shmuma/ptan
+"""
 
 import ptan
 import numpy as np
 import rospy
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 hidden_layer1_actor = rospy.get_param("/hidden_layer1_actor")
 hidden_layer2_actor = rospy.get_param("/hidden_layer2_actor")
-
 hidden_layer1_critic = rospy.get_param("/hidden_layer1_critic")
 hidden_layer2_critic = rospy.get_param("/hidden_layer2_critic")
 
-
-class DDPGActor(nn.Module): 
+class DDPGActor(nn.Module):
     def __init__(self, obs_size, act_size):
         super(DDPGActor, self).__init__()
 
@@ -52,22 +46,23 @@ class DDPGCritic(nn.Module):
         self.out_net = nn.Sequential(
             nn.Linear(hidden_layer1_critic + act_size, hidden_layer2_critic),
             nn.ReLU(),
-            nn.Linear(hidden_layer2_critic, 1) #1 corresponds to Q value 
+            nn.Linear(hidden_layer2_critic, 1)  # 1 corresponds to Q value
         )
 
     def forward(self, x, a):
         obs = self.obs_net(x)
-        return self.out_net(torch.cat([obs, a], dim=1)) #cat for catkin
+        return self.out_net(torch.cat([obs, a], dim=1))  # cat for catkin
 
-mu=rospy.get_param("/ou_mu")
-theta=rospy.get_param("/ou_theta")
-sigma=rospy.get_param("/ou_sigma")
-epsilon=rospy.get_param("/ou_epsilon")
+mu = rospy.get_param("/ou_mu")
+theta = rospy.get_param("/ou_theta")
+sigma = rospy.get_param("/ou_sigma")
+epsilon = rospy.get_param("/ou_epsilon")
 
 class AgentDDPG(ptan.agent.BaseAgent):
     """
     Exploration using Orstein-Uhlenbeck exploration process
     """
+
     def __init__(self, net, device="cpu", ou_enabled=True, ou_mu=mu, ou_teta=theta, ou_sigma=sigma, ou_epsilon=epsilon):
         self.net = net
         self.device = device
@@ -82,20 +77,21 @@ class AgentDDPG(ptan.agent.BaseAgent):
 
     def __call__(self, states, agent_states):
         states_v = ptan.agent.float32_preprocessor(states).to(self.device)
-        mu_v = self.net(states_v) #outputs deterministic actions
+        mu_v = self.net(states_v)  # outputs deterministic actions
         actions = mu_v.data.cpu().numpy()
 
         if self.ou_enabled and self.ou_epsilon > 0:
-            new_a_states = [] #next agent states
+            new_a_states = []  # next agent states
             for a_state, action in zip(agent_states, actions):
                 if a_state is None:
                     a_state = np.zeros(shape=action.shape, dtype=np.float32)
                 a_state += self.ou_teta * (self.ou_mu - a_state)
                 a_state += self.ou_sigma * np.random.normal(size=action.shape)
-                
-                action += self.ou_epsilon * a_state 
-                new_a_states.append(a_state) #new agent states
+
+                action += self.ou_epsilon * a_state
+                new_a_states.append(a_state)  # new agent states
         else:
             new_a_states = agent_states
 
-        return actions, new_a_states #new agent state is saved as it has the noise for the next step
+        # new agent state is saved as it has the noise for the next step
+        return actions, new_a_states
